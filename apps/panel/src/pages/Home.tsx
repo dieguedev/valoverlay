6a57fd73d26b1ff863037af4e6cc02d022d977a5
dieeegue <dieeegue2104@gmail.com>
2026-09-20 17:46:03 +0200

    refactor: Cambia title y description de Home

--- apps/panel/src/pages/Home.tsx
+++ apps/panel/src/pages/Home.tsx
@@ -1,15 +1,15 @@
-import Seo from '../components/Seo/Seo'
+import Seo from "../components/Seo/Seo";
 
 function Home() {
   return (
     <>
       <Seo
-        title="ValoVerlay — Overlays para tus partidas de Valorant"
-        description="ValoVerlay genera overlays personalizados con tus estadísticas de Valorant en tiempo real para tus stream y partidas."
+        title="Valoverlay | Overlay de OBS para Valorant"
+        description="Genera overlays personalizados con tus estadísticas de Valorant en tiempo real para tus streams."
       />
       <h1>Inicio</h1>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
