426908fd8a880c38042ad651c951fd23fc5eb447
dieeegue <dieeegue2104@gmail.com>
2026-09-20 18:22:13 +0200

    feat: Integra Header y Footer en PublicLayout

--- apps/panel/src/layouts/PublicLayout.tsx
+++ apps/panel/src/layouts/PublicLayout.tsx
@@ -1,5 +1,16 @@
 import { Outlet } from 'react-router-dom'
+import { Footer } from '../components/Footer/Footer'
+import { Header } from '../components/Header/Header'
+import styles from './PublicLayout.module.scss'
 
 export const PublicLayout: React.FC = () => {
-  return <Outlet />
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
 }
